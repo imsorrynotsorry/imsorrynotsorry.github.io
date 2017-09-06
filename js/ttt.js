e01e0acf4cb938c006ca5e26d3cf0fcf6e5ca08e
imsorrynotsorry <30579736+imsorrynotsorry@users.noreply.github.com>
2017-09-06 08:33:31 -0400

    Add files via upload

--- js/ttt.js
+++ js/ttt.js
@@ -1,5 +1,8 @@
 var turn = "X";
 var count= 0;
+var gameOver = false;
+var x_win = 0;
+var o_win = 0;
 
 
 var board = [
@@ -20,17 +23,165 @@ function clickTTT(r, c)
 	{
 		count ++; 
 		active[r][c] = false;
+		
 		board[r][c] = turn;
 		document.getElementById("cont"+r+c).innerHTML = turn;
-		if (count % 2 === 1)
+		
+		checkBoard();
+		
+		if (gameOver)
 		{
-			turn = "O";
+			clearBoard();
 		}
 		else
 		{
-			turn = "X";
+			if (count % 2 === 1)
+			{
+				turn = "O";
+			}
+			else
+			{
+				turn = "X";
+			}
+		}
+
+	}
+}
+
+function checkBoard()
+{
+	//check rows for winner
+	for (var r=0; r<3; r++)
+	{
+		if (board[r][0] == turn && board[r][1] == turn && board[r][2] == turn)
+		{
+			playSound('xando');
+			if (turn == "X")
+			{
+				x_win++;
+				document.getElementById("x-wins").innerHTML = "X Wins: " + x_win;
+			}
+			else
+			{
+				o_win++;
+				document.getElementById("o-wins").innerHTML = "O Wins: " + o_win;
+			}
+			alert(turn + ' wins!');
+			stopSound('xando');
+			gameOver = true;
 		}
 	}
+	
+	//check cols for winner
+	for (var c=0; c<3; c++)
+	{
+		if (board[0][c] == turn && board[1][c] == turn && board[2][c] == turn)
+		{
+			playSound('xando');
+			if (turn == "X")
+			{
+				x_win++;
+				document.getElementById("x-wins").innerHTML = "X Wins: " + x_win;
+			}
+			else
+			{
+				o_win++;
+				document.getElementById("o-wins").innerHTML = "O Wins: " + o_win;
+			}
+			alert(turn + ' wins!');
+			stopSound('xando');
+			gameOver = true;
+		}
+	}
+	
+	//check diagonals for winner
+	if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn)
+		{
+			playSound('xando');
+			if (turn == "X")
+			{
+				x_win++;
+				document.getElementById("x-wins").innerHTML = "X Wins: " + x_win;
+			}
+			else
+			{
+				o_win++;
+				document.getElementById("o-wins").innerHTML = "O Wins: " + o_win;
+			}
+			alert(turn + ' wins!');
+			stopSound('xando');
+			gameOver = true;
+		}
+	if (board[0][2] == turn && board[1][1] == turn && board[2][0] == turn)
+		{
+			playSound('xando');
+			if (turn == "X")
+			{
+				x_win++;
+				document.getElementById("x-wins").innerHTML = "X Wins: " + x_win;
+			}
+			else
+			{
+				o_win++;
+				document.getElementById("o-wins").innerHTML = "O Wins: " + o_win;
+			}
+			alert(turn + ' wins!');
+			stopSound('xando');
+			gameOver = true;
+		}
+	
+	//check tie
+	checkDraw();
+}
+
+function checkDraw()
+{
+	var emptyCount = 0;
+	
+	for (var r =0; r < 3; r++)
+	{
+		for (var c = 0; c < 3; c++)
+		{
+			if (board[r][c] == " ")
+			{
+				emptyCount ++;
+			}
+		}
+	}
+	
+	if (emptyCount == 0)
+	{
+		gameOver = true;
+		alert("Tie Game!");
+	}
+}
+
+
+function clearBoard()
+{
+	active = [
+		[true, true, true],
+		[true, true, true],
+		[true, true, true]
+		];
+		
+	board = [
+		[" "," ", " "],
+		[" "," ", " "],
+		[" "," ", " "]
+		];
+		
+	for (var r = 0; r < 3; r++)
+	{
+		for (var c = 0; c < 3; c++)
+		{
+			document.getElementById("cont"+r+c).innerHTML = "<br>";
+		}
+	}
+		
+	turn = "X";
+	count= 0;
+	gameOver = false;
 }
 
 function playSound(soundobj)
